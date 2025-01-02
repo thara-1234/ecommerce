211ea7a57df12b97d2408f43a61bf61534b49c8d
thara-1234 <tharakrishna1989@gmail.com>
2025-01-02 22:23:39 +0530

    An exception class MyGlobalExceptionHandler is added

--- src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
+++ src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.quickshop.onlinestore.controller;
 
 import com.quickshop.onlinestore.model.Category;
 import com.quickshop.onlinestore.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,7 @@ public class CategoryController {
         return new ResponseEntity<>(categories,HttpStatus.OK);
     }
     @PostMapping("/public/categories")
-    public ResponseEntity<String> addCategory(@RequestBody Category category){
+    public ResponseEntity<String> addCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         //return "Category added successfully";
         return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
